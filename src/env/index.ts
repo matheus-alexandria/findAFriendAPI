42d9e6677e4f3270123fa241724617755ccebc53
Matchobas <maalexandria@hotmail.com>
2023-10-21 16:00:53 -0300

    feat: create env validation with zod

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
+  PORT: z.coerce.number().default(3500)
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  console.log('Invalid environment variables.', parsedEnv.error.format());
+
+  throw new Error('Invalid environment variables.');
+}
+
+export const env = parsedEnv.data;
