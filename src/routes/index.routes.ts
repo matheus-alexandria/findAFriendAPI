29196c14259ca1fb89773aa330bf04b2650f58c1
Matchobas <maalexandria@hotmail.com>
2023-11-01 09:24:43 -0300

    feat: create first post route to create a new pet

---
+++ src/routes/index.routes.ts
@@ -0,0 +1,40 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function routes(app: FastifyInstance) {
+  app.post('/pet', async (request, response) => {
+    const createPetRequestSchema = z.object({
+      about: z.string(),
+      age: z.enum(['filhote', 'adulto', 'idoso']),
+      size: z.enum(['pequeno', 'medio', 'grande']),
+      energyLevel: z.enum(['baixa', 'media', 'alta']),
+      independencyLevel: z.enum(['baixa', 'media', 'alta']),
+      environment: z.enum(['house', 'open', 'apartment']),
+    });
+
+    const { 
+      about,
+      age,
+      size,
+      energyLevel,
+      independencyLevel,
+      environment
+    } = createPetRequestSchema.parse(request.body);
+
+    const pet = await prisma.pet.create({
+      data: {
+        about,
+        age,
+        size,
+        energy_level: energyLevel,
+        independency_level: independencyLevel,
+        environment
+      }
+    });
+    
+    return response.send({
+      pet
+    });
+  });
+}
