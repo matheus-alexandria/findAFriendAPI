77b31f0913ad700ce6aa485045ec80837791e408
Matchobas <maalexandria@hotmail.com>
2023-11-01 09:25:08 -0300

    feat: register routes with fastify and set error handler to catch zod errors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,26 @@
 import fastify from "fastify";
 import { env } from "./env";
+import { routes } from "./routes/index.routes";
+import { ZodError } from "zod";
 
 const server = fastify();
+
+server.register(routes);
+
+server.setErrorHandler((error, _, response) => {
+  if (error instanceof ZodError) {
+    return response.status(400).send({
+      message: 'Zod validation error.',
+      detail: error.format()
+    });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.log(error);
+  }
+
+  return response.status(500).send({ message: 'Internal server error.'});
+});
 
 server.listen(
   { 
