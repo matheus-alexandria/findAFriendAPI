5df9e413d1534704ab6796bf9437acfdd65f4862
Matchobas <maalexandria@hotmail.com>
2023-11-02 10:54:26 -0300

    feat: create organization controller

---
+++ src/controller/organizations/createOrganizationController.ts
@@ -0,0 +1,38 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { OrganizationsPrismaRepository } from "../../repositories/prisma/organizationsPrismaRepository";
+import { CreateOrganizationUseCase } from "../../useCases/createOrganizationUseCase";
+
+export class CreateOrganizationController {
+  async handle(request: FastifyRequest, response: FastifyReply) {
+    const createOrgRequestSchema = z.object({
+      email: z.string().email(),
+      cep: z.string().max(8).min(8),
+      address: z.string(),
+      cellphone: z.string().max(11),
+      password: z.string().min(6),
+    });
+
+    const {
+      email,
+      cep,
+      address,
+      cellphone,
+      password
+    } = createOrgRequestSchema.parse(request.body);
+
+    const organizationsPrismaRepository = new OrganizationsPrismaRepository();
+
+    const createOrganizationUseCase = new CreateOrganizationUseCase(organizationsPrismaRepository);
+
+    const organization = await createOrganizationUseCase.execute({
+      email,
+      cep,
+      address,
+      cellphone,
+      password
+    });
+
+    return response.send(organization);
+  }
+}
