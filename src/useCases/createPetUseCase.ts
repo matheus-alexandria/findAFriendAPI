14c45e0ba23ec425e4d18c96cd0e627e15f80d84
Matchobas <maalexandria@hotmail.com>
2023-11-03 11:00:21 -0300

    feat: create pet use case

---
+++ src/useCases/createPetUseCase.ts
@@ -0,0 +1,54 @@
+import { Pet } from "@prisma/client";
+import { PetsRepository } from "../repositories/petsRepository";
+import { OrganizationRepository } from "../repositories/organizationsRepository";
+
+export class CreatePetUseCase {
+  constructor(
+    private petsRepository: PetsRepository,
+    private organizationsRepository: OrganizationRepository
+  ) {}
+
+  async execute({
+    about,
+    age,
+    size,
+    energyLevel,
+    independencyLevel,
+    environment,
+    organizationId
+  }: CreatePetUseCaseRequest): Promise<CreatePetUseCaseResponse> {
+    const org = await this.organizationsRepository.findById(organizationId);
+
+    if (!org) {
+      throw new Error('Organization not found.');
+    }
+
+    const pet = await this.petsRepository.create({
+      about,
+      age,
+      size,
+      energy_level: energyLevel,
+      independency_level: independencyLevel,
+      environment,
+      organization_id: organizationId
+    });
+
+    return {
+      pet,
+    }
+  }
+}
+
+interface CreatePetUseCaseRequest {
+  about: string;
+  age: string;
+  size: string;
+  energyLevel: string;
+  independencyLevel: string;
+  environment: string;
+  organizationId: string;
+}
+
+interface CreatePetUseCaseResponse {
+  pet: Pet
+}
