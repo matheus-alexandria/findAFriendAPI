258b8f94beb45c775bd6ecd45b084bdedcbf56f4
Matchobas <maalexandria@hotmail.com>
2023-11-03 10:58:31 -0300

    fix: add async to organizationRoutes

--- src/controller/organizations/routes.ts
+++ src/controller/organizations/routes.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { CreateOrganizationController } from "./createOrganizationController";
 
-export function organizationRoutes(app: FastifyInstance) {
+export async function organizationRoutes(app: FastifyInstance) {
   const createOrganizationController = new CreateOrganizationController();
   app.post('/', createOrganizationController.handle);
 }
