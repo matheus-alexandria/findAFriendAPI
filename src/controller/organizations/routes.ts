affa2de2bdf741a0eddee8dd14bb12bdf7ecfb53
Matchobas <maalexandria@hotmail.com>
2023-11-04 13:26:44 -0300

    feat: add organizationLogin route to org routes

--- src/controller/organizations/routes.ts
+++ src/controller/organizations/routes.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { CreateOrganizationController } from "./createOrganizationController";
+import { OrganizationLoginController } from "./organizationLoginController";
 
 export async function organizationRoutes(app: FastifyInstance) {
   const createOrganizationController = new CreateOrganizationController();
   app.post('/', createOrganizationController.handle);
+
+  const organizationLoginController = new OrganizationLoginController();
+  app.post('/login', organizationLoginController.handle);
 }
