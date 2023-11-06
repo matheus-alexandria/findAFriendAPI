e04f31d513f12bd343d832ea8366b30b88f9773e
Matchobas <maalexandria@hotmail.com>
2023-11-06 11:08:10 -0300

    feat: controller for login route returning jw token

--- src/controller/organizations/organizationLoginController.ts
+++ src/controller/organizations/organizationLoginController.ts
@@ -1,7 +1,28 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { OrganizationsPrismaRepository } from "../../repositories/prisma/organizationsPrismaRepository";
+import { OrganizationLoginUseCase } from "../../useCases/organizationLoginUseCase";
 
 export class OrganizationLoginController {
   async handle(req: FastifyRequest, res: FastifyReply) {
-    return res.send();
+    const loginRequestSchema = z.object({
+      email: z.string().email(),
+      password: z.string(),
+    });
+
+    const { email, password } = loginRequestSchema.parse(req.body);
+
+    const organizationRepository = new OrganizationsPrismaRepository();
+    const organizationLogin = new OrganizationLoginUseCase(organizationRepository);
+
+    const { organization } = await organizationLogin.execute({ email, password });
+
+    const token = res.jwtSign({}, {
+      sub: organization.id
+    })
+
+    return res.send({
+      token
+    });
   }
 }
