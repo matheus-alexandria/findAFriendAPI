2f3c95a335037b7054507834152aafd658e94578
Matchobas <maalexandria@hotmail.com>
2023-11-09 10:41:28 -0300

    refactor: add try catch for better return error message in pet controller

--- src/controller/pets/createPetController.ts
+++ src/controller/pets/createPetController.ts
@@ -6,43 +6,51 @@ import { PetsPrismaRepository } from "../../repositories/prisma/petsPrismaReposi
 
 export class CreatePetController {
   async handle(req: FastifyRequest, res: FastifyReply) {
-    const createPetRequestSchema = z.object({
-      about: z.string(),
-      age: z.enum(['filhote', 'adulto', 'idoso']),
-      size: z.enum(['pequeno', 'medio', 'grande']),
-      energyLevel: z.enum(['baixa', 'media', 'alta']),
-      independencyLevel: z.enum(['baixa', 'media', 'alta']),
-      environment: z.enum(['casa', 'apartamento', 'aberto']),
-      organizationId: z.string().uuid()
-    });
-
-    const { 
-      about,
-      age,
-      size,
-      energyLevel,
-      independencyLevel,
-      environment,
-      organizationId
-    } = createPetRequestSchema.parse(req.body);
-
-    const petsRepository = new PetsPrismaRepository();
-    const organizationsRepository = new OrganizationsPrismaRepository();
-    const createPetUseCase = new CreatePetUseCase(
-      petsRepository,
-      organizationsRepository
-    );
-
-    const pet = await createPetUseCase.execute({
-      about,
-      age,
-      size,
-      energyLevel,
-      independencyLevel,
-      environment,
-      organizationId
-    });
-
-    return res.send(pet);
+    try {
+      const createPetRequestSchema = z.object({
+        about: z.string(),
+        age: z.enum(['filhote', 'adulto', 'idoso']),
+        size: z.enum(['pequeno', 'medio', 'grande']),
+        energyLevel: z.enum(['baixa', 'media', 'alta']),
+        independencyLevel: z.enum(['baixa', 'media', 'alta']),
+        environment: z.enum(['casa', 'apartamento', 'aberto']),
+        organizationId: z.string().uuid()
+      });
+  
+      const { 
+        about,
+        age,
+        size,
+        energyLevel,
+        independencyLevel,
+        environment,
+        organizationId
+      } = createPetRequestSchema.parse(req.body);
+  
+      const petsRepository = new PetsPrismaRepository();
+      const organizationsRepository = new OrganizationsPrismaRepository();
+      const createPetUseCase = new CreatePetUseCase(
+        petsRepository,
+        organizationsRepository
+      );
+  
+      const pet = await createPetUseCase.execute({
+        about,
+        age,
+        size,
+        energyLevel,
+        independencyLevel,
+        environment,
+        organizationId
+      });
+  
+      return res.send(pet);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).send({
+          message: err.message
+        });
+      }
+    }
   }
 }
