e56b53cd788958f66809956ee466e451a401df3e
Matchobas <maalexandria@hotmail.com>
2023-11-11 10:57:21 -0300

    feat: add get all pets to pet routes

--- src/controller/pets/routes.ts
+++ src/controller/pets/routes.ts
@@ -1,10 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { CreatePetController } from "./createPetController";
 import { authenticateToken } from "../../middlewares/authenticateToken";
+import { GetAllPetsController } from "./getAllPetsController";
 
 export async function petRoutes(app: FastifyInstance) {
   const createPetController = new CreatePetController();
   app.post('/', {
     preHandler: [authenticateToken]
   }, createPetController.handle);
+
+  const getAllPetsController = new GetAllPetsController();
+  app.get('/', getAllPetsController.handle);
 }
