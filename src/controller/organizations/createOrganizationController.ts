e98d846cbd090bf7516a32b53a5e0cd2b9cbb187
Matchobas <maalexandria@hotmail.com>
2023-11-10 23:23:56 -0300

    refactor: add try catch to create organization controller

--- src/controller/organizations/createOrganizationController.ts
+++ src/controller/organizations/createOrganizationController.ts
@@ -5,40 +5,48 @@ import { CreateOrganizationUseCase } from "../../useCases/createOrganizationUseC
 
 export class CreateOrganizationController {
   async handle(request: FastifyRequest, response: FastifyReply) {
-    const createOrgRequestSchema = z.object({
-      email: z.string().email(),
-      cep: z.string().max(8).min(8),
-      address: z.string(),
-      cellphone: z.string().max(11),
-      password: z.string().min(6),
-    });
-
-    const {
-      email,
-      cep,
-      address,
-      cellphone,
-      password
-    } = createOrgRequestSchema.parse(request.body);
-
-    const organizationsPrismaRepository = new OrganizationsPrismaRepository();
-    const createOrganizationUseCase = new CreateOrganizationUseCase(organizationsPrismaRepository);
-
-    const { organization } = await createOrganizationUseCase.execute({
-      email,
-      cep,
-      address,
-      cellphone,
-      password
-    });
-
-    const token = await response.jwtSign({}, {
-      sub: organization.id
-    });
-
-    return response.send({
-      token,
-      organization
-    });
+    try {
+      const createOrgRequestSchema = z.object({
+        email: z.string().email(),
+        cep: z.string().max(8).min(8),
+        address: z.string(),
+        cellphone: z.string().max(11),
+        password: z.string().min(6),
+      });
+  
+      const {
+        email,
+        cep,
+        address,
+        cellphone,
+        password
+      } = createOrgRequestSchema.parse(request.body);
+  
+      const organizationsPrismaRepository = new OrganizationsPrismaRepository();
+      const createOrganizationUseCase = new CreateOrganizationUseCase(organizationsPrismaRepository);
+  
+      const { organization } = await createOrganizationUseCase.execute({
+        email,
+        cep,
+        address,
+        cellphone,
+        password
+      });
+  
+      const token = await response.jwtSign({}, {
+        sub: organization.id
+      });
+  
+      return response.send({
+        token,
+        organization
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        return response.status(400).send({
+          message: err.message
+        });
+      }
+    }
   }
 }
