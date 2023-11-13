cf524c628f6b1720df321ac8ad37fe0bb1b12afc
Matchobas <maalexandria@hotmail.com>
2023-11-13 17:59:43 -0300

    feat: first filters and rest of controller implementation

--- src/controller/pets/getAllPetsController.ts
+++ src/controller/pets/getAllPetsController.ts
@@ -1,13 +1,28 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { GetAllPetsUseCase } from "../../useCases/getAllPetsUseCase";
 import { PetsPrismaRepository } from "../../repositories/prisma/petsPrismaRepository";
+import { z } from "zod";
 
 export class GetAllPetsController {
   async handle(req: FastifyRequest, res: FastifyReply) {
+    const getAllPetsQuerySchema = z.object({
+      age: z.enum(['filhote', 'adulto', 'idoso']),
+      size: z.enum(['pequeno', 'medio', 'grande']),
+    });
+
+    const {
+      age,
+      size
+    } = getAllPetsQuerySchema.parse(req.query);
+
     const petsRepository = new PetsPrismaRepository();
     const getAllPetsUseCase = new GetAllPetsUseCase(petsRepository);
 
-    const { pets } = await getAllPetsUseCase.execute();
+    const { pets } = await getAllPetsUseCase.execute({
+      age, 
+      size
+    });
+
     return res.send({
       pets
     });
