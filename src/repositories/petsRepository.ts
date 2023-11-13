2d565e8ad2110ccdac53a3393ee49f8352a6e871
Matchobas <maalexandria@hotmail.com>
2023-11-13 18:00:03 -0300

    feat: prisma repository getall method implemented

--- src/repositories/petsRepository.ts
+++ src/repositories/petsRepository.ts
@@ -1,5 +1,11 @@
 import { Pet, Prisma } from "@prisma/client";
 
+export interface PetFilters {
+  age?: string;
+  size?: string;
+}
+
 export interface PetsRepository {
+  getAllByFilters(filters?: PetFilters): Promise<Pet[]>;
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>;
 }
--- src/repositories/prisma/petsPrismaRepository.ts
+++ src/repositories/prisma/petsPrismaRepository.ts
@@ -1,8 +1,16 @@
 import { Pet, Prisma } from "@prisma/client";
-import { PetsRepository } from "../petsRepository";
+import { PetFilters, PetsRepository } from "../petsRepository";
 import { prisma } from "../../lib/prisma";
 
 export class PetsPrismaRepository implements PetsRepository {
+  async getAllByFilters(filters?: PetFilters): Promise<Pet[]> {
+    const pets = await prisma.pet.findMany({
+      where: filters
+    });
+
+    return pets;
+  }
+
   async create(data: Prisma.PetUncheckedCreateInput): Promise<Pet> {
     const pet = await prisma.pet.create({
       data,
