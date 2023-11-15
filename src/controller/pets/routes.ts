a52d61fc4eb4592dcca6c0ff6b3e75a77b29ff31
Matchobas <maalexandria@hotmail.com>
2023-11-15 10:43:55 -0300

    feat: add new find a pet route to pet routes

--- src/controller/pets/routes.ts
+++ src/controller/pets/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { CreatePetController } from "./createPetController";
 import { authenticateToken } from "../../middlewares/authenticateToken";
 import { GetAllPetsController } from "./getAllPetsController";
+import { FindPetController } from "./findPetController";
 
 export async function petRoutes(app: FastifyInstance) {
   const createPetController = new CreatePetController();
@@ -11,4 +12,7 @@ export async function petRoutes(app: FastifyInstance) {
 
   const getAllPetsController = new GetAllPetsController();
   app.get('/', getAllPetsController.handle);
+
+  const findPetController = new FindPetController();
+  app.get('/:petId', findPetController.handle);
 }
