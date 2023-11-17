47f88c8b4f08e4f3ce3372b0f4b166b921930d03
Matchobas <maalexandria@hotmail.com>
2023-11-17 11:03:20 -0300

    refactor: return org data and wrap in try catch

--- src/controller/organizations/organizationLoginController.ts
+++ src/controller/organizations/organizationLoginController.ts
@@ -5,24 +5,35 @@ import { OrganizationLoginUseCase } from "../../useCases/organizationLoginUseCas
 
 export class OrganizationLoginController {
   async handle(req: FastifyRequest, res: FastifyReply) {
-    const loginRequestSchema = z.object({
-      email: z.string().email(),
-      password: z.string(),
-    });
+    try {
+      const loginRequestSchema = z.object({
+        email: z.string().email(),
+        password: z.string(),
+      });
+  
+      const { email, password } = loginRequestSchema.parse(req.body);
+  
+      const organizationRepository = new OrganizationsPrismaRepository();
+      const organizationLogin = new OrganizationLoginUseCase(organizationRepository);
+  
+      const { organization } = await organizationLogin.execute({ email, password });
+  
+      const token = res.jwtSign({}, {
+        sub: organization.id
+      });
+  
+      return res.send({
+        organization,
+        token
+      });
+    } catch(err) {
+      if (err instanceof Error) {
+        return res.status(401).send({
+          message: err.message
+        });
+      }
 
-    const { email, password } = loginRequestSchema.parse(req.body);
-
-    const organizationRepository = new OrganizationsPrismaRepository();
-    const organizationLogin = new OrganizationLoginUseCase(organizationRepository);
-
-    const { organization } = await organizationLogin.execute({ email, password });
-
-    const token = res.jwtSign({}, {
-      sub: organization.id
-    });
-
-    return res.send({
-      token
-    });
+      throw err;
+    }
   }
 }
