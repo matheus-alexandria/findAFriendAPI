7d8c0d015912d5abe27d9053755880b4fbc574b8
Matchobas <maalexandria@hotmail.com>
2023-11-19 15:07:34 -0300

    feat: create route for getAllPetsByState

--- src/controller/pets/routes.ts
+++ src/controller/pets/routes.ts
@@ -3,6 +3,7 @@ import { CreatePetController } from "./createPetController";
 import { authenticateToken } from "../../middlewares/authenticateToken";
 import { GetAllPetsController } from "./getAllPetsController";
 import { FindPetController } from "./findPetController";
+import { GetAllPetsByStateController } from "./getAllPetsByStateController";
 
 export async function petRoutes(app: FastifyInstance) {
   const createPetController = new CreatePetController();
@@ -15,4 +16,7 @@ export async function petRoutes(app: FastifyInstance) {
 
   const findPetController = new FindPetController();
   app.get('/:petId', findPetController.handle);
+
+  const getAllPetsStateController = new GetAllPetsByStateController();
+  app.get('/:state', getAllPetsStateController.handle);
 }
