d6010045dfe067fcdf2d11cc90fd355e1090426d
Matchobas <maalexandria@hotmail.com>
2023-11-21 17:13:08 -0300

    refactor: merge get pets methods in a single findMany

--- src/repositories/petsRepository.ts
+++ src/repositories/petsRepository.ts
@@ -4,7 +4,6 @@ export type PetFilters = Partial<Omit<Pet, 'id'>>;
 
 export interface PetsRepository {
   findById(id: string): Promise<Pet | null>;
-  findManyByState(state: string): Promise<Pet[]>;
-  getAllByFilters(filters?: PetFilters): Promise<Pet[]>;
+  findMany(state: string, filters?: PetFilters): Promise<Pet[]>;
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>;
 }
