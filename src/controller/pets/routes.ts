010412acfd69121d9792ee32e39849587b1436c7
Matchobas <maalexandria@hotmail.com>
2023-11-21 17:14:21 -0300

    refactor: remove old getAllPetsByState and import

--- src/controller/pets/routes.ts
+++ src/controller/pets/routes.ts
@@ -3,7 +3,6 @@ import { CreatePetController } from "./createPetController";
 import { authenticateToken } from "../../middlewares/authenticateToken";
 import { GetAllPetsController } from "./getAllPetsController";
 import { FindPetController } from "./findPetController";
-import { GetAllPetsByStateController } from "./getAllPetsByStateController";
 
 export async function petRoutes(app: FastifyInstance) {
   const createPetController = new CreatePetController();
